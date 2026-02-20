c2cc0431d0513907d1a96cbe7fcf43299a378297
khuongvnn-dev <khuongvo2105@gmail.com>
2026-02-20 16:50:05 +0700

    feat: update Education content

--- app/components/Education.tsx
+++ app/components/Education.tsx
@@ -1,20 +1,30 @@
+import { cleanJsonStr, getDataByKey } from "@/helpers/GGSheetHelper";
+import TreeView from "./TreeView";
+import { RootState } from "@/lib/store";
+import { useSelector } from "react-redux";
+
 const Education = () => {
+
+  const { data, loading } = useSelector((state: RootState) => state.google_sheet);
+
+  const t = (key: string) => getDataByKey(data, 'vi', key);
+  
+  const careerPath = () => {
+    if (!loading && data && data.length > 0) {
+      try {
+        const careerData = JSON.parse(cleanJsonStr(t('career-path')));
+        return <TreeView items={careerData} title={t('h-background')} />;
+      } catch (err) {
+        console.error("Error parsing career path data:", err);
+      }
+    }
+  }
+
   return (
     <section id="background" className="py-10 scroll-mt-14 lg:scroll-mt-0">
-      <h2 className="text-4xl font-bold uppercase mb-10">Education</h2>
-      
-      <div className="flex flex-col md:flex-row justify-between items-start mb-8">
-        <div className="space-y-2">
-          <h3 className="text-2xl font-bold uppercase">Nong Lam University</h3>
-          <div className="text-xl font-medium text-gray-600">
-            Sinh viên năm <b>4</b> chuyên ngành Công nghệ thông tin (tính đến 2025)
-          </div>
-          <p className="text-gray-500 italic">Chương trình đại học 4 năm tập trung vào lập trình Java</p>
-        </div>
-        <div className="mt-2 md:mt-0">
-          <span className="text-sky-600 font-bold text-lg">2021 - Dự kiến 2025</span>
-        </div>
-      </div>
+      <h2 className="text-4xl font-bold uppercase mb-10">{t('h-background')}</h2>
+
+      {careerPath()}
     </section>
   );
 };
